058993fbdabc1df2802511adee4983feda35c36b
Angel Parraga <im.angelparraga@gmail.com>
2025-02-01 22:23:17 +0100

    wip

--- src/main/kotlin/parraga/bros/tournament/services/TournamentService.kt
+++ src/main/kotlin/parraga/bros/tournament/services/TournamentService.kt
@@ -1,5 +1,11 @@
 package parraga.bros.tournament.services
 
+import parraga.bros.tournament.domain.Player
+
 class TournamentService {
+
+    fun startTournament(players: List<Player>) {
+
+    }
 
 }
