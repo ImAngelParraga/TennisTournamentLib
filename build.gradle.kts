e52b0bf89126f3c41a7344925605c3c2159f1fdb
Ángel Párraga <im.angelparraga@gmail.com>
2026-04-22 09:39:32 +0200

    chore: bump version to 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "parraga.bros"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
     mavenCentral()
@@ -23,7 +23,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             group = "parraga.bros"
-            version = "0.0.1"
+            version = "0.0.3"
             artifactId = "TennisTournamentLib"
 
             from(components["java"])
